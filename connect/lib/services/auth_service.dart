11f61a5407d9249345eec0647f4bf0dc684b9c85
Moksh01 <shahmoksh001@gmail.com>
2024-01-31 22:35:07 +0530

    Role based user auth

--- connect/lib/services/auth_service.dart
+++ connect/lib/services/auth_service.dart
@@ -21,12 +21,37 @@ class AuthService {
     }
   }
 
+  Future logInTeacherWithEmailAndPassword(String email, String password) async {
+    try {
+      User? user = (await firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user;
+
+      if (user != null) {
+        await DatabaseService(uid: user.uid).updateTeacherData(email, password);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
   //logout
   Future signOut() async {
     try {
       await HelperFunction.saveUserLoggedInStatus(false);
       await HelperFunction.saveUserEmailSF("");
       await HelperFunction.saveUserNameSF("");
+      await firebaseAuth.signOut();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  Future techerSignOut() async {
+    try {
+      await HelperFunction.saveTeacherLoggedInStatus(false);
+      await HelperFunction.saveTeacherEmailSF("");
       await firebaseAuth.signOut();
     } catch (e) {
       return null;
---
+++ connect/lib/widgets/display.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class Display extends StatefulWidget {
+  const Display({super.key});
+
+  @override
+  State<Display> createState() => _DisplayState();
+}
+
+class _DisplayState extends State<Display> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
